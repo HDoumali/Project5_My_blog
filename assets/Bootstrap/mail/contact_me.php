c88c89f752afcd4619c0ce5f3849d9f2739c056f
HassanDoumali <doumalihassan4@yahoo.fr>
2018-04-30 14:39:08 +0200

    Update the file 'Contact_me.php'

--- assets/Bootstrap/mail/contact_me.php
+++ assets/Bootstrap/mail/contact_me.php
@@ -16,10 +16,10 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'doumalihassan4@yahoo.fr'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'contact@hdoumali.fr'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Formulaire de contact:  $name";
 $email_body = "Vous avez recu un nouveau message de votre blog.\n\n"."Voici les détails:\n\nNom: $name\n\nPrénom: $firstname\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: doumalihassan4@yahoo.fr\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: contact@hdoumali.fr\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
